37a5cb74e2a56c0f58e8a7a565cc6f11f1690415
Robin Vinterbladh <robin.vinterbladh@gmail.com>
2023-12-01 21:21:35 +0100

    Day 1 complete

--- Day1/main.kt
+++ Day1/main.kt
@@ -1,3 +1,25 @@
+import java.io.File
+
+var acumulate: Long = 0
+var numbers = listOf("1", "2", "3", "4", "5", "6", "7", "8", "9",
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+var numberMap: Map<String, String> = mapOf("one" to "1", "two" to "2", "three" to "3", "four" to "4", "five" to "5",
+    "six" to "6", "seven" to "7", "eight" to "8", "nine" to "9")
+
 fun main() {
-    println("Hello World!")
+    var infile = File("res/input1.txt")
+    infile.forEachLine{ findNumbers(it) }
+    println(acumulate)
+}
+
+fun findNumbers(line: String) {
+    var first: String = line.findAnyOf(strings = numbers, ignoreCase = true)?.second ?: ""
+    var last: String = line.findLastAnyOf(strings = numbers, ignoreCase = true)?.second ?: ""
+    first = numberMap.get(first) ?: first
+    last = numberMap.get(last) ?: last
+    try {
+        acumulate += (first + last).toInt()
+    } catch ( e: Exception) {
+        println("no element in " + line)
+    }
 }
