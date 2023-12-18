14ed4a386866156ba20d0759db1f8dd659c82726
Robin Vinterbladh <robin.vinterbladh@gmail.com>
2023-12-18 21:08:02 +0100

    day 18 part done

--- Day18/main.kt
+++ Day18/main.kt
@@ -4,6 +4,8 @@ var digList: MutableList<Dig> = mutableListOf()
 data class Dig(var dir: Char, var length: Int, var col: String)
 
 var digMap: MutableMap<Int,MutableSet<Pair<Int,String>>> = mutableMapOf()
+var digList: MutableList<Pair<Int,Int>> = mutableListOf()
+
 
 fun main() {
     var inFile = File("input.txt")
@@ -11,38 +13,49 @@ fun main() {
         var ins = it.split(' ')
         digList.add(Dig(ins[0].first(), ins[1].toInt(), ins[2])) 
     }
-    println(digList)
     var prev = Pair(0,0)
     for (dig in digList) {
         when(dig.dir) {
             'R' -> repeat(dig.length) { 
                 if (!digMap.containsKey(prev.first)) digMap.put(prev.first, mutableSetOf())
                 digMap.get(prev.first)?.add(Pair(prev.second,dig.col)) 
+                digList.add(prev)
                 prev = Pair(prev.first, prev.second+1)
             }
             'L' -> repeat(dig.length) { 
                 if (!digMap.containsKey(prev.first)) digMap.put(prev.first, mutableSetOf())
                 digMap.get(prev.first)?.add(Pair(prev.second,dig.col)) 
+                digList.add(prev)
                 prev = Pair(prev.first, prev.second-1)
             }
             'U' -> repeat(dig.length) { 
                 if (!digMap.containsKey(prev.first)) digMap.put(prev.first, mutableSetOf())
                 digMap.get(prev.first)?.add(Pair(prev.second,dig.col)) 
+                digList.add(prev)
                 prev = Pair(prev.first-1, prev.second)
             }
             'D' -> repeat(dig.length) { 
                 if (!digMap.containsKey(prev.first)) digMap.put(prev.first, mutableSetOf())
                 digMap.get(prev.first)?.add(Pair(prev.second,dig.col)) 
+                digList.add(prev)
                 prev = Pair(prev.first+1, prev.second)
             }
             else -> println("ERROR no such direction")
         }
     }
-    println(digMap)
     var size = 0
-    for(key in digMap.keys){
-        var row: List<Int> = digMap[key]?.map{it.first}!!.sorted()
-        size += row.last()+1 - row.first()
+    for (p in digList.zipWithNext()) {
+        var p1 = p.first
+        var p2 = p.second
+        size += (p1.first*p2.second) - (p1.second*p2.first)
     }
     println(size)
+    var b = digMap.values.map{ it.size }.sum()
+    var i = 0
+    for(key in digMap.keys) {
+        var row = digMap.get(key)!!.map{ it.first }.sorted()
+        var diff = row.last() - row.first()
+        i += diff+1 - row.size
+    }
+    size = i + (b/2) - 1
 }
