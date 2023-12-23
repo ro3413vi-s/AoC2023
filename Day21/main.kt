a55cc9ef5f23aea82cda67f0807f028419f8d12a
Robin Vinterbladh <robin.vinterbladh@gmail.com>
2023-12-23 02:31:46 +0100

    day 21 done

--- Day21/main.kt
+++ Day21/main.kt
@@ -4,13 +4,12 @@ var gardenSet: MutableSet<Pair<Int,Int>> = mutableSetOf()
 var start: Pair<Int,Int> = Pair(-1,-1)
 
 var possibleLocations: MutableSet<Pair<Int,Int>> = mutableSetOf()
-var finalLocations: MutableSet<Pair<Int,Int>> = mutableSetOf()
 
 var rows: Int = 0
 var cols: Int = 0
 
 fun main() {
-    var inFile = File("tinput.txt")
+    var inFile = File("input.txt")
     inFile.readLines().forEachIndexed{row, it -> 
         rows = row
         it.forEachIndexed{col, s -> 
@@ -23,40 +22,31 @@ fun main() {
     cols++
     // Part 1
     possibleLocations.add(start)
-    var nextLocations: MutableSet<Pair<Int,Int>> = mutableSetOf()
 
     for( i in 1..64) {
-        for(location in possibleLocations) {
-            nextLocations.addAll(getLocations(location))
-        }
-        possibleLocations.clear()
-        possibleLocations.addAll(nextLocations)
-        nextLocations.clear()
+        possibleLocations = possibleLocations.map{ getLocations(it) }.flatten().toMutableSet()
     }
     println(possibleLocations.size)
 
     // Part 2
     possibleLocations.clear()
-    nextLocations.clear()
     possibleLocations.add(start)
-    val steps = 5000
-    for(i in 1..steps) {
-        for(location in possibleLocations) {
-            nextLocations.addAll(getLocationsInf(location))
-        }
-        possibleLocations.clear()
-        possibleLocations.addAll(nextLocations)
-        if(steps % 2 != 0 && i % 2 != 0) {
-            // uneven number of steps
-            finalLocations.addAll(nextLocations)
-        } else if( steps % 2 == 0 && i % 2 == 0) {
-            // even number of steps
-            finalLocations.addAll(nextLocations)
-        }
-        nextLocations.clear()
+    var p1 = 0
+    var p2 = 0
+    var p3 = 0
+    var i = 0
+    while(p1 == 0) {
+        if (i % (rows) == 65) {
+			p1 = p2
+            p2 = p3
+            p3 = possibleLocations.size
+		}
+        possibleLocations = possibleLocations.map{ getLocationsInf(it) }.flatten().toMutableSet()
+        i++
     }
-    finalLocations.addAll(possibleLocations)
-    println(finalLocations.size)
+    var x = 26501365L / rows
+    println( p1 + x * (p2-p1 + (x-1) * ((p3-p2-p2+p1)/2) ) )
+
 }
 
 fun getLocations(location: Pair<Int,Int>): MutableList<Pair<Int,Int>> {
@@ -72,10 +62,7 @@ fun getLocations(location: Pair<Int,Int>): MutableList<Pair<Int,Int>> {
 }
 
 // Part 2
-// infinite map -> modulo the coords and check on initial map?
-// possible other speedups?
-// Every garden we reach after an uneven number of steps will be part of the final answer so we can ignore previously visited locations
-// if we remember these
+
 fun getLocationsInf(location: Pair<Int,Int>): MutableList<Pair<Int,Int>> {
     var temp: MutableList<Pair<Int,Int>> = mutableListOf()
     var u = Pair(location.first-1, location.second)
@@ -86,7 +73,7 @@ fun getLocationsInf(location: Pair<Int,Int>): MutableList<Pair<Int,Int>> {
         var t = Pair(p.first % rows, p.second % cols)
         if(t.first < 0) t = Pair(t.first + rows, t.second)
         if(t.second < 0) t = Pair(t.first, t.second + cols)
-        if(t in gardenSet && !(p in finalLocations)) temp.add(p)
+        if(t in gardenSet) temp.add(p)
     }
     return temp
 }
